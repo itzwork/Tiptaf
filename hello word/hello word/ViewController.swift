ad03db9396a1aea5ed04055d75958242d0c8e5f2
itzwork <44123113+itzwork@users.noreply.github.com>
2020-07-26 21:53:36 +0300

    Start project
    Hello word

---
+++ hello word/hello word/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello word
+//
+//  Created by admin on 12/05/2020.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
